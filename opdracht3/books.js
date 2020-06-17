0497781ee9ab7db55b64ebbd17ecf9c9c95293dc
IreneSchipper <64211399+IreneSchipper@users.noreply.github.com>
2020-06-17 11:50:48 +0200

    Add files via upload

--- opdracht3/books.js
+++ opdracht3/books.js
@@ -1,52 +1,53 @@
-const header = document.querySelector ('header');
-const section = document.querySelector ('section');
+const header = document.querySelector('header');
+const section = document.querySelector('section');
+const myArticle = document.createElement('article');
 
 
-let requestURL = 'https://raw.githubusercontent.com/dudeonthehorse/datasets/master/amazon.books.json'; 
-let request = new XMLHttpRequest(); 
-request.open('GET', requestURL); 
+let requestURL ='https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
 
-request.responseType = 'json'; 
-request.send(); 
+request.responseType = 'json';
+request.send();
 
-request.onload = function() { 
-  const books = request.response; 
-    
+request.onload = function () {
+    const books = request.response;
+
     console.log(books);
-   
+
     showBooks(books);
 }
 
-
+// Boeken overzicht
 
 // deze funtie wordt aangeroepen in de onload, hierin worden html elementen aangemaakt en json date gekoppeld
 function showBooks(jsonObj) {
     //De members worden nu geshowt in de console log
-    console.log (jsonObj);
-
-    
-  //const heroes = jsonObj['members']; 
-//console.log(jsonObj[4] .cover);   
-    
-    
-  for (let i = 0; i < jsonObj.length; i++) {
-      //html elementen aanmaken
-    const myH1 = document.createElement('h1');
-      myH1.textContent = jsonObj [i].title;
-      
-    //h2 element aanmaken en json data koppelen
-      
-      //img element aanmaken en json data koppelen
-      const plaatje = document.createElement('img');
-      plaatje.src = jsonObj [i].thumbnailUrl;
+    console.log(jsonObj);
 
 
-   //hier worden html elemnten genest
-    myArticle.appendChild(myH1);
-    myArticle.appendChild(plaatje);
+    //const heroes = jsonObj['members']; 
+    //console.log(jsonObj[4] .cover);   
 
-      /* hier worden nieuwe html elementen aan de section in de html toegevoegd*/   
-    section.appendChild(myArticle);
-  }
+
+    for (let i = 0; i < jsonObj.length; i++) {
+        //html elementen aanmaken
+        const myH1 = document.createElement('h1');
+        myH1.textContent = jsonObj[i].title;
+
+        //h2 element aanmaken en json data koppelen
+
+        //img element aanmaken en json data koppelen
+        const plaatje = document.createElement('img');
+        plaatje.src = jsonObj[i].imageLink;
+
+
+        //hier worden html elemnten genest
+        myArticle.appendChild(myH1);
+        myArticle.appendChild(plaatje);
+
+        /* hier worden nieuwe html elementen aan de section in de html toegevoegd*/
+        section.appendChild(myArticle);
+    }
 }
 
